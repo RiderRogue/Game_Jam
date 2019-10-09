c00b3423be3389ed1849346db4e4f6b0760cf171
shiba <kbc17b13@stu.kawahara.ac.jp>
2019-10-09 14:26:45 +0900

    遷移させるやつ追加

--- Gattac_Fang/Assets/Scenes/TitleScene/TitleDirector.cs
+++ Gattac_Fang/Assets/Scenes/TitleScene/TitleDirector.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;          //シーンマネジメントを有効にする
 
-public class ResultDirector : MonoBehaviour
+public class TitleDirector : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -16,7 +16,7 @@ public class ResultDirector : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0) || Input.GetKeyDown("space")) //マウス左クリック、スペースキーを押した場合
         {
-            SceneManager.LoadScene("TitleScene");//titleシーンをロードする
+            SceneManager.LoadScene("MainGameScene");//gameシーンをロードする
         }
     }
 }
