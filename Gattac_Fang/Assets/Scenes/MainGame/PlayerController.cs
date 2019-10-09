7b5eedf5dc6911f50210bef17fcf638e31b88fec
kbc19b31 <kbc19b31@stu.kawahara.ac.jp>
2019-10-09 11:08:53 +0900

    プレイヤーの移動処理を追加（キー入力なのでスティック入力に変更する。）

---
+++ Gattac_Fang/Assets/Scenes/MainGame/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左矢印が押された時
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-1, 0, 0); //左に「1」動かす
+        }
+        //右矢印が押された時
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(1, 0, 0); //右に「1」動かす
+        }
+        //上矢印が押された時
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 1, 0); //上に「1」動かす
+        }
+        //下矢印が押された時
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -1, 0); //下に「１」動かす
+        }
+    }
+}
