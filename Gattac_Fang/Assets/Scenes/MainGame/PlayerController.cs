24bb84d679b97b7b11622c9713003be0b43a4714
kbc19b31 <kbc19b31@stu.kawahara.ac.jp>
2019-10-09 13:52:07 +0900

    キャラ移動スティックにした

--- Gattac_Fang/Assets/Scenes/MainGame/PlayerController.cs
+++ Gattac_Fang/Assets/Scenes/MainGame/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class PlayerController : MonoBehaviour
 {
@@ -19,19 +20,29 @@ public class PlayerController : MonoBehaviour
             transform.Translate(-1, 0, 0); //左に「1」動かす
         }
         //右矢印が押された時
-        if(Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.Translate(1, 0, 0); //右に「1」動かす
         }
         //上矢印が押された時
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             transform.Translate(0, 1, 0); //上に「1」動かす
         }
         //下矢印が押された時
-        if(Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             transform.Translate(0, -1, 0); //下に「１」動かす
         }
+
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+       this.transform.position += new Vector3(x/2, y/2,0);
+        float xr = Input.GetAxisRaw("Horizontal2");
+        float yr = Input.GetAxisRaw("Vertical2");
+        float z = xr * yr;
+        this.transform.rotation =  new Quaternion(0,0,z,1);
+
     }
 }
