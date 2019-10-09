903283fadc6a5e4b7efac6f9738258ca77520f9f
shiba <kbc17b13@stu.kawahara.ac.jp>
2019-10-09 16:37:29 +0900

    遷移するときの使用変更

---
+++ Gattac_Fang/Assets/Scenes/TitleScene/TextUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  ////ここを追加////
+
+public class TextUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Text>().text = "PRESS ANY BUTTON";
+    }
+}
