58f66a1c994d462788f07aed02563b18b6e86cd8
YanoTaku <kbc17b24@stu.kawahara.ac.jp>
2019-10-09 13:48:30 +0900

    ScoraText

---
+++ Gattac_Fang/Assets/Scenes/Result/ScoraText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  ////ここを追加////
+
+public class ScoraText : MonoBehaviour
+{
+    //スコアを格納する。
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Text>().text = "総得点　" + score.ToString() + "点";
+    }
+}
